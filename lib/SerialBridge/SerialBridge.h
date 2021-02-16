500445d002e6e73854b75a16e8b9580bf8cdf87a
arnoson <omasocke@hotmail.de>
2021-02-16 16:17:33 +0100

    Restructure code

--- lib/SerialBridge/SerialBridge.h
+++ lib/SerialBridge/SerialBridge.h
@@ -2,85 +2,138 @@
 #define SerialBridge_h
 
 #include <SPI.h>
+#include <SD.h>
 
 #define START_OF_HEADER 0x01
 #define START_OF_TEXT 0x02
 #define END_OF_TRANSMISSION 0x04
+#define ENQUIRY 0x05
 
 class SerialBridge {
+  private:
+  // The buffer is used to store the command header (command name and
+  // argument).Additional content is not stored in the buffer but written
+  // directly to a file.
+  char buffer[1024];
+  int bufferPosition = -1;
+  bool isReceivingFile = false;
+
+  File fileSend;
+  File fileReceive;
+
+  enum MessageTypes {
+    MessageSuccess = (int) 'S',
+    MessageError = (int) 'E',
+    MessageReadFile = (int) 'R',
+    MessageWriteFile = (int) 'W'
+  };
+
+  typedef void(*CommandHandler)(int command, int id, const char* argument);
+  CommandHandler commandHandler;
+
   public:
-  typedef void(*CommandHandler)(int command, const char* argument);
-  typedef void(*ContentHandler)(uint8_t);
-  typedef void(*ContentEndHandler)(void);
-
+  /**
+   * Read and parse the serial for commands.
+   */
   void update() {
-    static char buffer[1024];
-    static int bufferPosition = -1;
-
     if (Serial.available()) {
       char data = Serial.read();
 
       if (data == START_OF_HEADER) {
-        // Reset the buffer.
+        // A new message begins.
         bufferPosition = 0;
         buffer[0] = (char) 0;
-      } else if (data == START_OF_TEXT) {
-        // Parse and reset the header.
+      } else if (data == END_OF_TRANSMISSION && isReceivingFile) {
+        // When we are receiving a file, an `END_OF_TRANSMISSION` signals that
+        // the content is finished.
+        if (fileReceive) fileReceive.close();
+        isReceivingFile = false;
+      } else if (data == START_OF_TEXT || data == END_OF_TRANSMISSION) {
+        // The message header ends (either with a `START_OF_TEXT`, if additional
+        // file content follows, or an `END_OF_TRANSMISSION`), so we can parse
+        // the header.
         buffer[bufferPosition++] = (char) 0;
-        bufferPosition = -1;
+        isReceivingFile = (data == START_OF_TEXT);
         int command = atoi(strtok(buffer, " "));
+        int id = atoi(strtok(NULL, " "));
         const char* argument = strtok(NULL, " ");
-        if (commandHandler) commandHandler(command, argument);
-      } else if (data == END_OF_TRANSMISSION) {
-        if (contentEndHandler) contentEndHandler();
-      } else if (bufferPosition >= 0) {
-        // Fill the header.
+        handleCommand(command, id, argument);
+      } else if (isReceivingFile) {
+        // Write content to file.
+        if (fileReceive) fileReceive.write(data);
+      } else if (bufferPosition < 1024 - 1) {
+        // Fill the command buffer.
         buffer[bufferPosition++] = data;
-      } else {
-        if (contentHandler) contentHandler(data);
       }
-    }    
+    }
   }
 
-  // ? Send resource?
-  void sendCommand(int command, const char* argument) {
+  /**
+   * Handle the command. `CommandWriteFile = nd `CommandReadFile` are reserved
+   * for handling files, all other commands are passed to the command handler.
+   */
+  void handleCommand(int command, int id, const char* argument) {
+    if (command == CommandReadFile) {
+      sendHeader(command, id);
+      sendFile(argument);
+    } else if (command == CommandWriteFile) {
+      receiveFile(argument);
+    } else if (commandHandler) {
+      commandHandler(command, id, argument);
+    }
+  }
+
+  /**
+   * Send a command header to the serial.
+   */
+  void sendHeader(int command, const char* argument) {
     Serial.write(START_OF_HEADER);
     Serial.printf("%i %s", command, argument);
+  }
+  
+  void sendHeader(int command, int argument) {
+    Serial.write(START_OF_HEADER);
+    Serial.printf("%i %i", command, argument);
+  }
+
+  /**
+   * Send a command to the serial.
+   */
+  void sendCommand(int command, const char* argument) {
+    sendHeader(command, argument);
     Serial.write(END_OF_TRANSMISSION);
   }
 
-  void sendCommand(int command, const char* argument, File content) {
-    sendCommand(command, argument);
-    while (content.available()) {
-      Serial.write(content.read());
-    }
-    content.close();
-  }
-
-  void startContent() {
+  /**
+   * Send a file to the serial.
+   */
+  void sendFile(const char* fileName) {
     Serial.write(START_OF_TEXT);
-  }
 
-  void endContent() {
+    fileSend = SD.open(fileName);
+    while (fileSend.available()) {
+      Serial.write(fileSend.read());
+    }
+    fileSend.close();
+
     Serial.write(END_OF_TRANSMISSION);
   }
 
+  /**
+   * Begin receiving a file through the serial. The file's content is then 
+   * written asynchronously in the `update()` method.
+   */
+  void receiveFile(const char* fileName) {
+    if (SD.exists(fileName)) SD.remove(fileName);
+    fileReceive = SD.open(fileName, FILE_WRITE);
+  }
+
+  /**
+   * Register a command handler.
+   */
   void onCommand(CommandHandler handler) {
     commandHandler = handler;
   }
-
-  void onContent(ContentHandler handler) {
-    contentHandler = handler;
-  }
-
-  void onContentEnd(ContentEndHandler handler) {
-    contentEndHandler = handler;
-  }
-  
-  private:
-  CommandHandler commandHandler;
-  ContentHandler contentHandler;
-  ContentEndHandler contentEndHandler;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,8 @@
 #include <SD.h>
 #include <SPI.h>
 #include "SerialBridge.h"
-#include "FileTransfer.h"
 
 SerialBridge bridge;
-
-enum Commands {
-  CommandWrite,
-  CommandRead,
-  CommandWriteFinished,
-  CommandFile
-};
 
 void setup() {
   Serial.begin(9600);
@@ -20,27 +12,8 @@ void setup() {
   if (!SD.begin(BUILTIN_SDCARD)) {
     Serial.println("Failed to initialize SD card!");
   }
-
-  bridge.onCommand(handleCommand);
-
-  fileTransfer::onWriteFinished([&](char* fileName) {
-    bridge.sendCommand(CommandWriteFinished, fileName);
-    Serial.write(END_OF_TRANSMISSION);
-  });
-}
-
-void handleCommand(int command, const char* argument) {
-  if (command == CommandWrite) {
-    fileTransfer::beginWrite(argument);
-  } else if (command == CommandRead) {
-    bridge.sendCommand(CommandFile, argument, fileTransfer::openForRead);
-    // bridge.startContent();
-    // fileTransfer::read(argument);
-    // bridge.endContent();
-  }
 }
 
 void loop() {
   bridge.update();
-  fileTransfer::update();
 }
