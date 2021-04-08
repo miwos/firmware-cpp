2d8ad674e505e78d10b6998ffcbfa823b4d1be18
arnoson <omasocke@hotmail.de>
2021-04-08 14:23:05 +0200

    Add MiwosBridge

--- lib/MiwosBridge/MiwosBridge.cpp
+++ lib/MiwosBridge/MiwosBridge.cpp
@@ -10,3 +10,80 @@ void MiwosBridge::sendMessage(OSCMessage &message) {
   slipSerial->endPacket();
   message.empty();
 }
+
+void MiwosBridge::handleOscInput(OSCBundle &oscInput) {
+  // oscInput.dispatch("/read-file", handleReadFile);
+  oscInput.dispatch("/file", startWriteFile);  
+}
+
+void MiwosBridge::startWriteFile(OSCMessage &fileMessage) {
+  readSerialMode = ReadSerialModeFile;
+  fileMessage.getString(0, fileNameWrite, fileNameLength);
+  // Always override old content with new content.
+  if (SD.exists(fileNameWrite)) SD.remove(fileNameWrite);
+  fileWrite = SD.open(fileNameWrite, FILE_WRITE);
+}
+
+void MiwosBridge::endWriteFile() {
+  readSerialMode = ReadSerialModeOSC;
+  fileWrite.close();
+  OSCMessage message("/success/file");
+  message.add(fileNameWrite);
+  sendMessage(message);
+}
+
+void MiwosBridge::update() {
+  OSCBundle oscInput;
+  bool receivedData = false;
+
+  if (slipSerial->available()) {
+    while (!slipSerial->endOfPacket()) {
+      while (slipSerial->available()) {
+        int c = slipSerial->read();
+        receivedData = true;
+        if (readSerialMode == ReadSerialModeOSC) {
+          oscInput.fill(c);
+        } else if (readSerialMode == ReadSerialModeFile) {
+          fileWrite.write(c);
+        }
+      }
+    }
+  }
+
+  if (receivedData && readSerialMode == ReadSerialModeOSC) {
+    handleOscInput(oscInput);
+  } else if (receivedData && readSerialMode == ReadSerialModeFile) {
+    endWriteFile();
+  }
+}
+
+const char* MiwosBridge::getLogAddress(LogType type) {
+  switch (type) {
+    case LogTypeInfo:
+      return "/log/info";
+    case LogTypeWarning:
+      return "/log/warning";
+    case LogTypeError:
+      return "/log/error";
+    default:
+      return "/undefined";
+  }
+}
+
+void MiwosBridge::log(LogType type, const char* text) {
+  OSCMessage message(getLogAddress(type));
+  message.add(text);
+  sendMessage(message);
+}
+
+void MiwosBridge::info(const char* text) {
+  log(LogTypeInfo, text);
+}
+
+void MiwosBridge::warning(const char* text) {
+  log(LogTypeWarning, text);
+}
+
+void MiwosBridge::error(const char* text) {
+  log(LogTypeError, text);
+}  
--- lib/MiwosBridge/MiwosBridge.h
+++ lib/MiwosBridge/MiwosBridge.h
@@ -1,16 +1,54 @@
 #ifndef MiwosBridge_h
 #define MiwosBridge_h
 
+#include <SPI.h>
+#include <SD.h>
 #include <SLIPSerial.h>
 #include <OSCMessage.h>
+#include <OSCBundle.h>
 
 class MiwosBridge {
+private:
+  enum ReadSerialMode {
+    ReadSerialModeOSC,
+    ReadSerialModeFile
+  };
+
+  enum LogType {
+    LogTypeInfo,
+    LogTypeWarning,
+    LogTypeError
+  };
+
+  File fileRead;
+  File fileWrite;
+
+  static const byte fileNameLength = 50;
+  char fileNameRead[fileNameLength];
+  char fileNameWrite[fileNameLength];
+
+  ReadSerialMode readSerialMode = ReadSerialModeOSC;
+
+  void handleOscInput(OSCBundle &oscInput);
+
+  void startWriteFile(OSCMessage &fileMessage);
+  void endWriteFile();
+
+  const char* getLogAddress(LogType type);
+  void log(LogType type, const char* text);
+
 public:
   SLIPSerial* slipSerial;
 
   MiwosBridge(SLIPSerial *slipSerial);
   void sendMessage(OSCMessage &message);
   void sendRaw();
+
+  void update();
+
+  void info(const char* text);
+  void warning(const char* text);
+  void error(const char* text);
 };
 
 #endif
