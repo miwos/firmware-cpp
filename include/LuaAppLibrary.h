052d9e9811943d275858367082395f5f5bf310cf
arnoson <omasocke@hotmail.de>
2022-03-17 21:06:16 +0100

    Send any osc message from lua

--- include/LuaAppLibrary.h
+++ include/LuaAppLibrary.h
@@ -14,10 +14,30 @@ int selectPart(lua_State *L) {
   return 0;
 }
 
+int sendMessage(lua_State *L) {
+  int argsCount = lua_gettop(L);
+  const char *name = luaL_checkstring(L, 1);
+  OSCMessage message(name);
+
+  for (int i = 2; i <= argsCount; i++) {
+    if (lua_isboolean(L, i)) {
+      message.add(lua_toboolean(L, i));
+    } else if (lua_isnumber(L, i)) {
+      message.add(lua_tonumber(L, i));
+    } else if (lua_isstring(L, i)) {
+      message.add(lua_tostring(L, i));
+    }
+  }
+
+  bridge->sendMessage(message);
+  return 0;
+}
+
 void install(LuaOnArduino *loa) {
   LuaAppLibrary::bridge = &(loa->bridge);
 
-  const luaL_reg library[] = {{"selectPart", selectPart}, {NULL, NULL}};
+  const luaL_reg library[] = {
+      {"selectPart", selectPart}, {"sendMessage", sendMessage}, {NULL, NULL}};
   loa->lua.registerLibrary("App", library);
 }
 } // namespace LuaAppLibrary
