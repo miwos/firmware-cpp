9a1dd9c654df370bdb4c4d053b9e5ecf41077f4d
arnoson <omasocke@hotmail.de>
2022-04-07 20:04:42 +0200

    Adjust encoder range on runtime

--- include/LuaEncodersLibrary.h
+++ include/LuaEncodersLibrary.h
@@ -10,11 +10,21 @@ Encoders *encoders;
 Bridge *bridge;
 
 int write(lua_State *L) {
-  byte index = lua_tonumber(L, 1) - 1; // Use zero-based index.
+  byte index = lua_tonumber(L, 1) - 1; // zero-based index.
   int32_t value = lua_tonumber(L, 2);
 
   RangeEncoder *encoder = encoders->getEncoder(index);
   if (encoder != NULL) encoder->write(value);
+  return 0;
+}
+
+int setRange(lua_State *L) {
+  byte index = luaL_checknumber(L, 1) - 1; // zero-based index.
+  int32_t min = luaL_checkinteger(L, 2);
+  int32_t max = luaL_checkinteger(L, 3);
+
+  RangeEncoder *encoder = encoders->getEncoder(index);
+  if (encoder != NULL) encoder->setRange(min, max);
   return 0;
 }
 
@@ -45,8 +55,8 @@ void install(LuaOnArduino *loa, Encoders *encoders) {
   LuaEncodersLibrary::encoders = encoders;
   LuaEncodersLibrary::bridge = &(loa->bridge);
 
-  const luaL_reg library[] = {
-      {"write", write}, {"selectPage", selectPage}, {NULL, NULL}};
+  const luaL_reg library[] = {{"write", write}, {"setRange", setRange},
+      {"selectPage", selectPage}, {NULL, NULL}};
   lua->registerLibrary("Encoders", library);
 
   encoders->onChange(handleChange);
--- lib/RangeEncoder/RangeEncoder.h
+++ lib/RangeEncoder/RangeEncoder.h
@@ -10,6 +10,17 @@ private:
   int32_t max = 127;
   int32_t value = 0;
 
+  int32_t constrainValue(int32_t v) {
+    if (v < min) {
+      v = 0;
+      encoder->write(min - 1);
+    } else if (v > max) {
+      v = max;
+      encoder->write(max + 1);
+    }
+    return v;
+  }
+
 public:
   RangeEncoder(uint8_t pin1, uint8_t pin2, int32_t min, int32_t max) {
     encoder = new Encoder(pin1, pin2);
@@ -17,18 +28,14 @@ public:
     this->max = max;
   }
 
+  void setRange(int32_t min, int32_t max) {
+    this->min = min;
+    this->max = max;
+    value = constrainValue(value);
+  }
+
   int32_t read() {
-    int32_t newValue = encoder->read();
-
-    if (newValue < min) {
-      newValue = 0;
-      encoder->write(min - 1);
-    } else if (newValue > max) {
-      newValue = max;
-      encoder->write(max + 1);
-    }
-
-    value = newValue;
+    value = constrainValue(encoder->read());
     return value;
   }
 
